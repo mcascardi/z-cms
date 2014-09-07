5eaf66326d8f2d3de6a0a99b21f3d0d101bee8bb
Matt Cascardi <mcascard@mcascard-7240-0.ad.checkpoint.com>
2014-09-06 17:00:36 -0700

    Added doc comment and version # for initial release

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Bootstrap file
+ * 
+ * Loads the dependencies and sets up classes/objects needed to run
+ * the core of the application.
+ *
+ * PHP Version 5
+ *
+ * @since 0.1
+ */
+
 define('DS', DIRECTORY_SEPARATOR);
 include __DIR__ . '/lib/functions.php';
 include __DIR__ . '/classes/SplClassLoader.php';
